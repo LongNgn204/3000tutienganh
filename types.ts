e92d9593cc9189825441c4b2236b31199cfdf3de
Long Nguyễn <stu725114073@hnue.edu.vn>
2025-10-26 18:28:11 +0700

    git push

--- types.ts
+++ types.ts
@@ -13,8 +13,11 @@ export interface Category {
   words: Word[];
 }
 
+export type CEFRLevel = 'A1' | 'A2' | 'B1' | 'B2' | 'C1' | 'C2';
+
 export interface User {
   name: string;
+  level: CEFRLevel;
 }
 
 export type StudyStatus = 'known' | 'review';
